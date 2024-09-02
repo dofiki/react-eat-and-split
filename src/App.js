ec1b0abf4940cabc768252f71a3526c169e72a59
Dixit Regmi <dixitregmiofficial@gmail.com>
2024-09-02 19:05:29 +0545

    updated the left side
    done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const friendData = [
   {id:1, name:"Ram", image:"./user.png", balance: 0},
   {id:2, name:"Shyam", image:"./user.png", balance: 5},
@@ -5,21 +7,43 @@ const friendData = [
 ]
 
 export default function App(){
-  return <div>
-    <FriendList />
+
+  const[addForm,setAddForm] = useState(false);
+
+  function handleAddFriend(){
+    setAddForm(!addForm);
+  }
+  
+  
+  return <div className="all">
+    <div className="left">
+    <FriendList addForm={addForm} handleAddFriend={handleAddFriend}/>
+    <div className="addBtnContainer">
+    <button 
+    className="addBtn"
+    onClick={handleAddFriend}
+    style={addForm?{display:"none"}:{}}
+    >
+      Add Friend</button>
+    </div>
+    <FriendForm addForm={addForm} handleAddFriend={handleAddFriend} />
+    </div >
+    <SplitForm />
   </div>
 }
 
-function FriendList(){
-  return <div className="friendList">
+
+function FriendList({addForm,handleAddFriend}){
+  return <div className="">
     {friendData.map((user)=>{
       return <Friend 
       key={user.id} 
       name={user.name} 
       image={user.image}
       balance={user.balance}/>
+
+      
   })}
-  <div className="addBtnContainer"><button className="addBtn">Add Friend</button></div>
   </div>
 }
 
@@ -45,4 +69,31 @@ function Friend({name, image, balance}){
    </div>
     <button className="selectBtn">select</button>
   </div>
+}
+
+function FriendForm({addForm, handleAddFriend}){
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return <div className="friendForm" style={addForm?{display:"flex"}:{}}>
+    <form onSubmit={handleSubmit}>
+    <div className="top">
+      <label for="inpName">Friend Name: </label>
+      <input name="inpName" type="text" className="inpName"></input>
+    </div>
+    <div className="bottom">
+      <label for="inpURL">Image URL:  </label>
+      <input name="inpName" type="text" className="inpURL"></input>
+    </div>
+    <div className="addBtnContainer"><button className="addBtn">Add</button></div>
+    <button className="closeBtn" onClick={(e)=>handleAddFriend(e.preventDefault)}>close</button>
+    </form>
+  
+  </div>
+}
+
+function SplitForm(){
+  return<div className="right">Eat and Split:</div>
 }
