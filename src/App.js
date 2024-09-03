82c4e68f59529e141a601c6687c1f36f35f4d98a
Dixit Regmi <dixitregmiofficial@gmail.com>
2024-09-03 16:34:11 +0545

    new user rendering feature
    done

--- src/App.js
+++ src/App.js
@@ -9,33 +9,39 @@ const friendData = [
 export default function App(){
 
   const[addForm,setAddForm] = useState(false);
+  const[addFren,setAddFren] = useState(friendData);
 
   function handleAddFriend(){
     setAddForm(!addForm);
   }
-  
+
+  function handleAddFren(newFriend){
+    setAddFren((friendData)=>[...friendData,newFriend]);
+    setAddForm(false);
+  }
   
   return <div className="all">
+
     <div className="left">
-    <FriendList addForm={addForm} handleAddFriend={handleAddFriend}/>
-    <div className="addBtnContainer">
-    <button 
-    className="addBtn"
-    onClick={handleAddFriend}
-    style={addForm?{display:"none"}:{}}
-    >
-      Add Friend</button>
-    </div>
-    <FriendForm addForm={addForm} handleAddFriend={handleAddFriend} />
+
+      <FriendList addForm={addForm} handleAddFriend={handleAddFriend} addFren={addFren} />
+
+      <div className="addBtnContainer"><button  className="addBtn"
+      onClick={handleAddFriend} style={addForm?{display:"none"}:{}}>Add Friend</button>
+      </div>
+
+      <FriendForm addForm={addForm} handleAddFriend={handleAddFriend} handleAddFren={handleAddFren}/>
+
     </div >
+
     <SplitForm />
+
   </div>
 }
 
-
-function FriendList({addForm,handleAddFriend}){
+function FriendList({addFren}){
   return <div className="">
-    {friendData.map((user)=>{
+    {addFren.map((user)=>{
       return <Friend 
       key={user.id} 
       name={user.name} 
@@ -71,29 +77,76 @@ function Friend({name, image, balance}){
   </div>
 }
 
-function FriendForm({addForm, handleAddFriend}){
+function FriendForm({addForm, handleAddFriend, handleAddFren}){
+ 
+  const [nam, setNam] = useState("");
+  const [tasbir, setTasbir] = useState("./user.png");
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    if(!nam || !tasbir ) return;
+
+    const newFriend = {
+      id: crypto.randomUUID(), 
+      name: nam, 
+      image: tasbir, 
+      balance: 0,
+    };
+
+    handleAddFren(newFriend);
+    console.log(newFriend);
+    setNam("");
+    setTasbir("./user.png");
   }
+
 
   return <div className="friendForm" style={addForm?{display:"flex"}:{}}>
     <form onSubmit={handleSubmit}>
     <div className="top">
       <label for="inpName">Friend Name: </label>
-      <input name="inpName" type="text" className="inpName"></input>
+      <input type="text" className="inpName" id="inpName" value={nam} onChange={(e)=>setNam(e.target.value)}></input>
     </div>
     <div className="bottom">
       <label for="inpURL">Image URL:  </label>
-      <input name="inpName" type="text" className="inpURL"></input>
+      <input  type="text" className="inpURL" id="inpURL" value={tasbir} onChange={(e)=>setTasbir(e.target.value)}></input>
     </div>
-    <div className="addBtnContainer"><button className="addBtn">Add</button></div>
-    <button className="closeBtn" onClick={(e)=>handleAddFriend(e.preventDefault)}>close</button>
+    <div className="addBtnContainer"><button className="addBtn" >Add</button></div>
+    <button className="closeBtn" onClick={handleAddFriend}>close</button>
     </form>
   
   </div>
 }
 
 function SplitForm(){
-  return<div className="right">Eat and Split:</div>
+  return<div className="right">
+    <h2>SPLIT A BILL WITH X</h2>
+    <form className="splitForm">
+      <div>
+      <label for="billValue">Bill value:  </label>
+      <input id="billValue" type="number" className="billInp"></input>
+      </div>
+      <div>
+      <label for="yourExpense">Your expense:  </label>
+      <input id="yourExpense" type="number" className="billInp"></input>
+      </div>
+      <div>
+      <label for="xExpense">X's expense:  </label>
+      <input id="xExpense" type="number" className="billInp"></input>
+      </div>
+
+     <div className="choose">
+     <label for="users" >Who is paying the bill ?</label>
+      <select name="users" id="chooseUser">
+        <option value="Rita">Rita</option>
+        <option value="you">you</option>
+      </select>
+     </div>
+
+      <div className="splitBtnContainer">
+        <button className="splitBtn">Split bill</button>
+      </div>
+
+    </form>
+  </div>
 }
