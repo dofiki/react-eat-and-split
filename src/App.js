8713df5c36dc76cbecb940fbd70f5827f324ef19
Dixit Regmi <dixitregmiofficial@gmail.com>
2024-09-04 19:17:20 +0545

    added select friend feature
    done

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export default function App(){
 
   const[addForm,setAddForm] = useState(false);
   const[addFren,setAddFren] = useState(friendData);
+  const[selectedFriend, setSelectFriend] = useState(null);
 
   function handleAddFriend(){
     setAddForm(!addForm);
@@ -19,14 +20,20 @@ export default function App(){
     setAddFren((friendData)=>[...friendData,newFriend]);
     setAddForm(false);
   }
+
+  function handleSelectedFriend(friend){
+    setSelectFriend((curr) => (curr && curr.id === friend.id ? null : friend));
+  }
   
   return <div className="all">
 
     <div className="left">
 
-      <FriendList addForm={addForm} handleAddFriend={handleAddFriend} addFren={addFren} />
+      <FriendList addForm={addForm} handleAddFriend={handleAddFriend} 
+                  addFren={addFren} onSelection={handleSelectedFriend}
+                  selectedFriend={selectedFriend} />
 
-      <div className="addBtnContainer"><button  className="addBtn"
+      <div className="addBtnContainer"><button className="addBtn"
       onClick={handleAddFriend} style={addForm?{display:"none"}:{}}>Add Friend</button>
       </div>
 
@@ -34,46 +41,48 @@ export default function App(){
 
     </div >
 
-    <SplitForm />
+    { selectedFriend && <SplitForm selectedFriend={selectedFriend}/>}
 
   </div>
 }
 
-function FriendList({addFren}){
+function FriendList({addFren, onSelection, selectedFriend}){
+
   return <div className="">
     {addFren.map((user)=>{
       return <Friend 
-      key={user.id} 
-      name={user.name} 
-      image={user.image}
-      balance={user.balance}/>
-
-      
+      key={user.id}
+      user={user}
+      onSelection={onSelection}
+      selectedFriend={selectedFriend}/>
   })}
   </div>
 }
 
-function Friend({name, image, balance}){
+function Friend({user, onSelection, selectedFriend }){
 
   function handleBalance(bal){
     if(bal===0){
       return <p className="userDescription" >you are all clear</p>
     }else if(bal<0){
-      return <p className="userDescription" style={{color:"red"}} >you owe {name} ${Math.abs(bal)}.</p>
+      return <p className="userDescription" style={{color:"red"}} >you owe {user.name} ${Math.abs(bal)}.</p>
     }else{
-      return <p className="userDescription" style={{color:"green"}} >{name} owes you ${Math.abs(bal)}.</p>
+      return <p className="userDescription" style={{color:"green"}} >{user.name} owes you ${Math.abs(bal)}.</p>
     }
   }
 
-  return <div className="eachFriend">
+
+  const isSelected = selectedFriend && selectedFriend.id === user.id;
+
+  return <div className="eachFriend" style={isSelected?{backgroundColor:" rgb(255, 238, 196)"}:{}}>
    <div className="userInfo">
-   <img src={image} alt="profile-photo" className="userImage"/>
+   <img src={user.image} alt="profile-photo" className="userImage"/>
     <div className="userInfoText">
-      <h3 className="userName">{name}</h3>
-      {handleBalance(balance)}
+      <h3 className="userName">{user.name}</h3>
+      {handleBalance(user.balance)}
     </div>
    </div>
-    <button className="selectBtn">select</button>
+    <button className="selectBtn" onClick={()=>onSelection(user)}>{isSelected?"Close":"Select"}</button>
   </div>
 }
 
@@ -95,7 +104,7 @@ function FriendForm({addForm, handleAddFriend, handleAddFren}){
     };
 
     handleAddFren(newFriend);
-    console.log(newFriend);
+
     setNam("");
     setTasbir("./user.png");
   }
@@ -104,11 +113,11 @@ function FriendForm({addForm, handleAddFriend, handleAddFren}){
   return <div className="friendForm" style={addForm?{display:"flex"}:{}}>
     <form onSubmit={handleSubmit}>
     <div className="top">
-      <label for="inpName">Friend Name: </label>
+      <label htmlFor="inpName">Friend Name: </label>
       <input type="text" className="inpName" id="inpName" value={nam} onChange={(e)=>setNam(e.target.value)}></input>
     </div>
     <div className="bottom">
-      <label for="inpURL">Image URL:  </label>
+      <label htmlFor="inpURL">Image URL:  </label>
       <input  type="text" className="inpURL" id="inpURL" value={tasbir} onChange={(e)=>setTasbir(e.target.value)}></input>
     </div>
     <div className="addBtnContainer"><button className="addBtn" >Add</button></div>
@@ -118,27 +127,27 @@ function FriendForm({addForm, handleAddFriend, handleAddFren}){
   </div>
 }
 
-function SplitForm(){
+function SplitForm({selectedFriend}){
   return<div className="right">
-    <h2>SPLIT A BILL WITH X</h2>
+    <h2>SPLIT A BILL WITH {selectedFriend.name.toUpperCase()}</h2>
     <form className="splitForm">
       <div>
-      <label for="billValue">Bill value:  </label>
+      <label htmlFor="billValue">Bill value:  </label>
       <input id="billValue" type="number" className="billInp"></input>
       </div>
       <div>
-      <label for="yourExpense">Your expense:  </label>
+      <label htmlFor="yourExpense">Your expense:  </label>
       <input id="yourExpense" type="number" className="billInp"></input>
       </div>
       <div>
-      <label for="xExpense">X's expense:  </label>
+      <label htmlFor="xExpense">{selectedFriend.name}'s expense:  </label>
       <input id="xExpense" type="number" className="billInp"></input>
       </div>
 
      <div className="choose">
-     <label for="users" >Who is paying the bill ?</label>
+     <label htmlFor="users" >Who is paying the bill ?</label>
       <select name="users" id="chooseUser">
-        <option value="Rita">Rita</option>
+        <option value="Rita">{selectedFriend.name}</option>
         <option value="you">you</option>
       </select>
      </div>
